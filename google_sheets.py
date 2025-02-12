fb8b5166d562914068a3a87fe232dbae62629d25
Muhammad AHSAN <ahsankhetran72@gmail.com>
2025-02-12 21:25:25 +0500

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Google Sheets API Setup
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
-CREDS_FILE = 'time-table-project-450013-7023b4e6dd12.json'  # Update this with your downloaded JSON
+CREDS_FILE = 'time-table-project-450013-d1f3b8e418b1.json'  # Update this with your downloaded JSON
 
 def get_google_sheets_data(sheet_url):
     creds = ServiceAccountCredentials.from_json_keyfile_name(CREDS_FILE, SCOPES)
