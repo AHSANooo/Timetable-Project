b7d05a605874ab81f28b724ecb57ce2279649a8c
Muhammad AHSAN <ahsankhetran72@gmail.com>
2025-02-13 00:54:03 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google_sheets import get_google_sheets_data
 from extract_timetable import get_timetable
 
-SHEET_URL = "https://docs.google.com/spreadsheets/d/YOUR_SHEET_ID/edit#gid=0"
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1dk0Raaf9gtbSdoMAGZal3y4m1kwr7UiuulxFxDKpM8Q/edit?gid=1882612924#gid=1882612924"
 
 def main():
     st.title("📅 FAST-NUCES FCS Timetable System")
