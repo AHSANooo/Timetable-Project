6948541a50daca0b9bf709e2f3162f32e4661e30
Muhammad AHSAN <ahsankhetran72@gmail.com>
2025-02-13 15:53:54 +0500

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -1,15 +1,16 @@
-import streamlit as st
-import gspread
 from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
 
 
 def get_google_sheets_data(sheet_url):
-    """Fetch Google Sheets file as a gspread object."""
-    credentials_dict = st.secrets["google_service_account"]
-    creds = Credentials.from_service_account_info(credentials_dict, scopes=["https://spreadsheets.google.com/feeds",
-                                                                            "https://www.googleapis.com/auth/drive"])
+    scopes = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+    creds = Credentials.from_service_account_file('path/to/credentials.json', scopes=scopes)
+    service = build('sheets', 'v4', credentials=creds)
 
-    client = gspread.authorize(creds)
-    sheet = client.open_by_url(sheet_url)
+    spreadsheet_id = sheet_url.split('/d/')[1].split('/')[0]
+    spreadsheet = service.spreadsheets().get(
+        spreadsheetId=spreadsheet_id,
+        includeGridData=True
+    ).execute()
 
-    return sheet
+    return spreadsheet
