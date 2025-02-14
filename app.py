4ea560ca349a7d8571c761bd64c088f848ff7156
Muhammad AHSAN <ahsankhetran72@gmail.com>
2025-02-14 20:21:46 +0500

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def main():
     st.title("📅 FAST-NUCES FCS Timetable System")
 
     # Fetch full spreadsheet data
-    st.info("Data Fetched from Timetable")
+    st.info("Fetched the data from Timetable.")
     try:
         spreadsheet = get_google_sheets_data(SHEET_URL)
     except Exception as e:
